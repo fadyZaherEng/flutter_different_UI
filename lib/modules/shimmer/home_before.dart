386d73990e4e05a157990afe67310126c559d6df
fadyZaherEng <fady33zaher@gmail.com>
2023-06-22 23:32:03 +0300

    first

--- lib/modules/shimmer/home_before.dart
+++ lib/modules/shimmer/home_before.dart
@@ -46,3 +46,52 @@ class HomeScreenBefore extends StatelessWidget {
     );
   }
 }
+//another way wrap list not item
+// import 'package:flutter/material.dart';
+// import 'package:shimmer/shimmer.dart';
+//
+// class HomeScreenBefore extends StatelessWidget {
+//   const HomeScreenBefore({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Shimmer.fromColors(
+//       baseColor: Colors.black87,
+//       highlightColor: Colors.cyanAccent,
+//       child:ListView.builder(
+//         itemCount: 20,
+//         shrinkWrap: true,
+//         itemBuilder: (ctx, idx) {
+//           return const Center(
+//             child: Card(
+//               elevation: 5,
+//               shadowColor: Colors.white,
+//               color: Colors.purple,
+//               margin: EdgeInsets.all(8),
+//               child: Padding(
+//                 padding: EdgeInsets.all(8.0),
+//                 child: Column(
+//                   children: [
+//                     Text(
+//                       "Welcome",
+//                       style: TextStyle(
+//                         fontWeight: FontWeight.bold,
+//                         fontSize: 15,
+//                         color: Colors.white,
+//                       ),
+//                     ),
+//                     SizedBox(height: 10,),
+//                     Image(image: AssetImage("assets/images/avatar.PNG"),
+//                       fit: BoxFit.fill,
+//                       width: double.infinity,
+//                       height: 120,),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
